a1b067d669ca96912efb8802fc8d927bdce88838
Mario Filjak <mario.filjak@transcom.com>
2024-02-27 23:05:43 +0100

    feat: chat-completition calls custom server

--- src/pages/api/chat-completion.ts
+++ src/pages/api/chat-completion.ts
@@ -1,12 +1,42 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
+import {  NextApiResponse } from "next"
 
 export const config = {
   runtime: 'edge'
 }
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  return new Response("Response text")
+const handler = async (req: Request, res: NextApiResponse) => {
+  try {
+
+    const serverUrl = process.env.SERVER_URL;
+
+    const jsonData = await req.json();
+    const { messages } = jsonData
+    let query = "";
+
+    if (messages.length > 0) {
+      query = messages[messages.length - 1]["content"];
+    } else {
+      query = messages[0]["content"];
+    }
+    
+    const response = await fetch(`${serverUrl}`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "query": query
+      }
+      )
+    });
+
+    const data = await response.json();
+
+    return new Response(`<div>${data["answer"]}</div><h3>Izvor</h3><div>${data["context"]}</div>`)
+  } catch (error) {
+    console.log("Error: ", error);
+    return new Response(`<div><h4 class="error-title">Došlo je do pogreške prilikom obrade upita. Molim Vas pokušajte ponovno.</h4><div class="error-message-box">${error}</div></div>`)
+  }
 }
 
 export default handler
